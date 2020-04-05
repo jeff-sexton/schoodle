96f11424c6584cd02bc0b19e7d5af0476ca34f5b
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-04-05 13:12:14 -0700

    added more seed users and votes

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
 -- Users table seeds here (Example)
 INSERT INTO users (id, name, email) VALUES (1001, 'Alice', 'alice@alice.com');
 INSERT INTO users (id, name, email) VALUES (1002, 'Kira', 'Kira@kira.com');
+INSERT INTO users (id, name, email) VALUES (1003, 'Edward', 'edward@edward.com');
+INSERT INTO users (id, name, email) VALUES (1004, 'Shane', 'shane@shane.com');
