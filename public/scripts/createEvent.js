37fe8abdb837194ce1e9752d16b3ea720f25a289
jeff-sexton <sexton.jeffp@gmail.com>
2020-05-29 13:22:44 -0700

    Fixing error added during unapproved pull request

--- public/scripts/createEvent.js
+++ public/scripts/createEvent.js
@@ -78,10 +78,8 @@ const createEvent = function (event) {
     return false;
   });
 
-  $.post('/events', $(this).serialize())
-
-  console.log($(this).serialize())
-    .then(({ event }) => { //more data is being passed if we switch to single page application...
+  $.post("/events", $(this).serialize()).then(({ event }) => {
+    //more data is being passed if we switch to single page application...
 
       // event.url contains the string URL to redirect to
       window.location.href = `/events/${event.url}`;
